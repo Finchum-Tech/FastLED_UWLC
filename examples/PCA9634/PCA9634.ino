b0070e271875ac5f5ff6cc740ad406f74f673cfa
averydesktop <>
2025-05-04 14:49:44 -0400

    added debug statements

--- examples/PCA9634/PCA9634.ino
+++ examples/PCA9634/PCA9634.ino
@@ -3,11 +3,11 @@
 /// @example PCA9634.ino
 
 #include <Arduino.h> //needed for conversion to cpp file
-#include <PCA9634FakePin.h>
 #include <FastLED.h> 
 #include <PCA9634.h>
 
 #define LED_PIN NULL
+#define OUTPUT_PIN 16
 
 // How many leds in your strip?
 #define NUM_LEDS 1
@@ -15,30 +15,50 @@
 // Define the array of leds
 CRGB ledArray[NUM_LEDS];
 
-// Define PCA9634 device --> Constuctor takes in I2C address TODO: Confirm this with Avery again but I think 0x00
-PCA9634 ledDriver(0x00); 
+// Define PCA9634 device --> Constuctor takes in I2C address of d115
+PCA9634 ledDriver(115); 
 
 //function prototype
 void updateLEDs();
 
 void setup() { 
+  
+
+    // Initialize serial communication at esp8266 native baud rate of 74880
+    Serial.begin(74880);
+    // Print a message to the Serial Monitor
+    Serial.println("Serial communication initialized.");
+
     // Uncomment/edit one of the following lines for your leds arrangement.
     // ## Clockless types ##
     FastLED.addLeds<WS2812, LED_PIN, GRB>(ledArray, NUM_LEDS); // dummy setup
 
+    //Start 2wire comm with the following pins 
+    Wire.begin(4, 5);
+    
+    //enable led
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
 
-    ledDriver.begin();
+    //enable output pin
+    pinMode(OUTPUT_PIN, OUTPUT);
+    digitalWrite(OUTPUT_PIN, HIGH);
+
+    ledDriver.begin(0x01,0x14); //start pca control with register mode values
+
+    Serial.println("setup loop initialized.");
 }
 
 void loop() { 
   // Turn the LED on, then pause
   ledArray[0] = CRGB::Red;
   updateLEDs();
-  delay(500);
+  delay(1000);
   // Now turn the LED off, then pause
   ledArray[0] = CRGB::Black;
   updateLEDs();
-  delay(500);
+  delay(1000);
+  Serial.println(millis());
 }
 
 
